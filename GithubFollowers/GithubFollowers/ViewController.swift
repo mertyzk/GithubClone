e98ec409c105255d99aa1b8f9c5aca86a4808db5
RinKusChi <meyuzak@gmail.com>
2022-09-13 09:53:32 +0300

    Delete StoryBoard & Set Images for Assets

---
+++ GithubFollowers/GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Macbook Air on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
