86245a3987349b353e159c8233494b3bb2e41235
RinKusChi <meyuzak@gmail.com>
2022-09-16 10:22:09 +0300

    Model Created - Planning Network

---
+++ GithubFollowers/GithubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Macbook Air on 16.09.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
