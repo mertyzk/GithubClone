ec38f94faf2e623b56aa8d9f73963effd2c55378
RinKusChi <meyuzak@gmail.com>
2022-09-18 11:57:08 +0300

    ARC & Memory Leaks & Weak Self

--- GithubFollowers/GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/GithubFollowers/Screens/FollowerListVC.swift
@@ -9,10 +9,8 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
-    enum Section {
-        case main
-    }
-    
+    enum Section { case main }
+
     var userName: String!
     var followers: [Follower] = []
     var collectionView: UICollectionView!
@@ -37,28 +35,15 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        let width                       = view.bounds.width
-        let padding: CGFloat            = 12
-        let minimumItemSpacing: CGFloat = 10
-        let avaibleWidth                = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth                   = avaibleWidth / 3
-        
-        let flowLayout                  = UICollectionViewFlowLayout()
-        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
-    
     func getFollowers(){
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case .success(let followers):
                 self.followers = followers
